9901de6afe341181727bb803cd6bdc79f1dc0915
apexsalesai <tim@apexsalesai.com>
2025-04-04 16:57:56 -0400

    Initial files

---
+++ apexsalesai_website/apexsales_website/assets/js/main.js
@@ -0,0 +1,7 @@
+// Mobile menu toggle
+document.querySelector('.mobile-menu-button').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
+
+// Console confirmation
+console.log('APEXsalesAI is running!');
